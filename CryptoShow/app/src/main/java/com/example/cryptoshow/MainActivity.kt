6d81f8c83de9f51bd61bfdc365c93898a378c138
Bojidar Qnkov <bojidar.qnkov@gmail.com>
2022-04-08 20:49:03 +0300

    Reformat MainActivity

--- CryptoShow/app/src/main/java/com/example/cryptoshow/MainActivity.kt
+++ CryptoShow/app/src/main/java/com/example/cryptoshow/MainActivity.kt
@@ -1,10 +1,8 @@
 package com.example.cryptoshow
 
 import android.graphics.Color
-import android.opengl.Visibility
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
 import android.view.View
 import android.widget.Button
 import android.widget.TextView
@@ -17,62 +15,78 @@ import retrofit2.Callback
 import retrofit2.Response
 
 class MainActivity : AppCompatActivity() {
+    private var searchField: TextInputEditText? = null
+    private var coinDetailsContainer: CardView? = null
+    private var coinName: TextView? = null
+    private var coinPrice: TextView? = null
+    private var coinPriceChange: TextView? = null
+    private var coinPriceLow: TextView? = null
+    private var coinPriceHigh: TextView? = null
+    private var searchButton: Button? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val searchField = findViewById<TextInputEditText?>(R.id.searchField)
-        val coinDetailsContainer = findViewById<CardView?>(R.id.coinDetailsContainer)
-        val coinName = findViewById<TextView?>(R.id.coinName)
-        val coinPrice = findViewById<TextView?>(R.id.coinPrice)
-        val coinPriceChange = findViewById<TextView?>(R.id.coinPriceChange)
-        val coinPriceLow = findViewById<TextView?>(R.id.coinPriceLow)
-        val coinPriceHigh = findViewById<TextView?>(R.id.coinPriceHigh)
-        val searchButton = findViewById<Button?>(R.id.searchButton)
+        searchField = findViewById(R.id.searchField)
+        coinDetailsContainer = findViewById(R.id.coinDetailsContainer)
+        coinName = findViewById(R.id.coinName)
+        coinPrice = findViewById(R.id.coinPrice)
+        coinPriceChange = findViewById(R.id.coinPriceChange)
+        coinPriceLow = findViewById(R.id.coinPriceLow)
+        coinPriceHigh = findViewById(R.id.coinPriceHigh)
+        searchButton = findViewById(R.id.searchButton)
 
-        searchButton.setOnClickListener {
-            searchButton.isEnabled = false
-            HttpClient.getHttpClient().getCoin(searchField.text?.toString()).enqueue(object: Callback<CoinResponse> {
-                override fun onResponse(call: Call<CoinResponse>, response: Response<CoinResponse>) {
-                    if (response.isSuccessful)
-                    {
-                        val coinResponse = response.body()
+        setListeners()
+    }
 
-                        coinDetailsContainer.visibility = View.VISIBLE
-                        coinName.text = coinResponse?.name
-                        coinPrice.text = "Price: $${coinResponse?.marketData?.currentPrice?.usd}"
-
-                        val priceChange = coinResponse?.marketData?.priceChangePercentage24h
-                        priceChange?.let {
-                            when {
-                                it < 0 -> {
-                                    coinPriceChange.setTextColor(Color.RED)
-                                }
-                                it > 0 -> {
-                                    coinPriceChange.setTextColor(Color.GREEN)
-                                }
-                                else -> {
-                                    coinPriceChange.setTextColor(Color.BLACK)
-                                }
-                            }
-                            coinPriceChange.text = it.toString()
-                        }
-
-                        coinPriceLow.text = "Low(24h): $${coinResponse?.marketData?.low?.usd}"
-                        coinPriceHigh.text = "High(24h): $${coinResponse?.marketData?.high?.usd}"
-                    }
-                    else {
-                        coinDetailsContainer.visibility = View.GONE
-                    }
-                    searchButton.isEnabled = true
-                }
-
-                override fun onFailure(call: Call<CoinResponse>, t: Throwable) {
-                    searchButton.isEnabled = true
-                    coinDetailsContainer.visibility = View.GONE
-                }
-
-            })
+    private fun setListeners(){
+        searchButton?.setOnClickListener {
+            searchButton?.isEnabled = false
+            handleSearchButtonClick()
+            searchButton?.isEnabled = true
         }
+    }
+
+    private fun handleSearchButtonClick(){
+        HttpClient.getHttpClient().getCoin(searchField?.text.toString()).enqueue(object: Callback<CoinResponse> {
+            override fun onResponse(call: Call<CoinResponse>, response: Response<CoinResponse>) {
+                if (response.isSuccessful){
+                    setCoinInfo(response.body())
+                }
+                else {
+                    coinDetailsContainer?.visibility = View.GONE
+                }
+            }
+
+            override fun onFailure(call: Call<CoinResponse>, t: Throwable) {
+                coinDetailsContainer?.visibility = View.GONE
+            }
+        })
+    }
+
+    private fun setCoinInfo(coinResponse: CoinResponse?){
+        coinDetailsContainer?.visibility = View.VISIBLE
+        coinName?.text = coinResponse?.name
+        coinPrice?.text = "Price: $${coinResponse?.marketData?.currentPrice?.usd}"
+
+        val priceChange = coinResponse?.marketData?.priceChangePercentage24h
+        priceChange?.let {
+            when {
+                it < 0 -> {
+                    coinPriceChange?.setTextColor(Color.RED)
+                }
+                it > 0 -> {
+                    coinPriceChange?.setTextColor(Color.GREEN)
+                }
+                else -> {
+                    coinPriceChange?.setTextColor(Color.BLACK)
+                }
+            }
+            coinPriceChange?.text = it.toString()
+        }
+
+        coinPriceLow?.text = "Low(24h): $${coinResponse?.marketData?.low?.usd}"
+        coinPriceHigh?.text = "High(24h): $${coinResponse?.marketData?.high?.usd}"
     }
 }
